dfa5751ad1939891c55a57ff9cb7bb65711ed8a7
Rohini Sangam R <quic_rohinisa@quicinc.com>
2023-03-08 18:29:13 +0530

    Test gerrit to check WF of System and Vendor Circular dependencies.
    Change-Id: Ie32198d0e5f86112d74046de59a4588b7281b9c7

--- base.mk
+++ base.mk
@@ -3,7 +3,7 @@ TARGET_KERNEL_VERSION ?= $(shell ls -1r kernel | grep "msm-*" | sed 's/msm-//' |
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
 
-# Set TARGET_USES_NEW_ION for 4.14 and higher kernels
+# Set TARGET_USES_NEW_ION for 4.14 and higher kernels.
 ifeq ($(TARGET_KERNEL_VERSION),$(filter $(TARGET_KERNEL_VERSION),3.18 4.4 4.9))
 TARGET_USES_NEW_ION := false
 else
