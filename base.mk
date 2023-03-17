298656f40d227a021b0eda24a7a28e0708b0afbd
Pooja Singh <quic_spooja@quicinc.com>
2023-03-17 11:15:45 +0530

    Enable Servicetracker AIDL
    Change-Id: I5c0c6be0b026f990164ededde49ae24a6ce10f2d

--- base.mk
+++ base.mk
@@ -826,6 +826,11 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.servicetracker@1.2-impl \
     vendor.qti.hardware.servicetracker@1.2-service
 
+#servicetracker AIDL
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.servicetrackeraidl-impl \
+    vendor.qti.hardware.servicetrackeraidl-service
+
 #debugApp FDA
 PRODUCT_PACKAGES += FDA
 PRODUCT_PACKAGES += fda.script.rc
