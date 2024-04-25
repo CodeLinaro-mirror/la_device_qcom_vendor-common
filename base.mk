7f062edd93241b3a1ed2f79e04645ee646c14b7d
Shaoqi,Li <quic_shaoli@quicinc.com>
2024-04-25 19:22:00 +0800

    Monaco: Enable qcom-system-daemon for wearable target
    Change-Id: Icf84b5dc880b763dc28f0c233a306a5bbc09c9ab

--- base.mk
+++ base.mk
@@ -1059,12 +1059,17 @@ else
   $(warning **********)
 endif
 
-ifeq ($(TARGET_HAS_LOW_RAM),true)
-    PRODUCT_PROPERTY_OVERRIDES += \
-        persist.vendor.qcomsysd.enabled=0
+ifeq ($(TARGET_SUPPORTS_WEARABLES),true)
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.qcomsysd.enabled=1
 else
-    PRODUCT_PROPERTY_OVERRIDES += \
-        persist.vendor.qcomsysd.enabled=1
+ifeq ($(TARGET_HAS_LOW_RAM),true)
+     PRODUCT_PROPERTY_OVERRIDES += \
+         persist.vendor.qcomsysd.enabled=0
+else
+     PRODUCT_PROPERTY_OVERRIDES += \
+         persist.vendor.qcomsysd.enabled=1
+endif
 endif
 
 PRODUCT_PACKAGES_DEBUG += \
