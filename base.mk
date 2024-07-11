80f7bdd901494267d3b3a80ea5c79d3773b684b7
Eruvaram Kumar Raja Reddy <quic_erajared@quicinc.com>
2024-07-11 10:37:06 +0530

    video : Disable video services for qti memory optimizations
    - Disable qcom codec libraries for memory optimization
    - Disable qcom video service from configuration files
    - Disable qcom video libraries for optimization
    
    Change-Id: I3b4baf6948fd648ecaa194af5d5771066619eabf

--- base.mk
+++ base.mk
@@ -884,7 +884,9 @@ PRODUCT_PACKAGES += $(LOC_API)
 PRODUCT_PACKAGES += $(MEDIA_PROFILES)
 PRODUCT_PACKAGES += $(MM_AUDIO)
 PRODUCT_PACKAGES += $(MM_CORE)
+ifneq ($(TARGET_HAS_QTI_OPTIMIZATIONS), true)
 PRODUCT_PACKAGES += $(MM_VIDEO)
+endif #TARGET_HAS_QTI_OPTIMIZATIONS
 PRODUCT_PACKAGES += $(OPENCORE)
 PRODUCT_PACKAGES += $(PPP)
 PRODUCT_PACKAGES += $(PROTOBUF)
@@ -986,7 +988,11 @@ endif
 
 ifneq ($(TARGET_ENABLE_QC_AV_ENHANCEMENTS),true)
 PRODUCT_COPY_FILES += \
+ifeq ($(TARGET_HAS_QTI_OPTIMIZATIONS),true)
+    device/qcom/common/media/media_codecs_opt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+else
     device/qcom/common/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+endif #TARGET_HAS_QTI_OPTIMIZATIONS
     device/qcom/common/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 endif
 
