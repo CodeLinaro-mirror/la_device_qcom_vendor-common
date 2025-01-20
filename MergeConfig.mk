c5a7cb01ea4d00cfdf95c424cd22c429e86a388f
kodanda rami reddy v <quic_kreddyv@quicinc.com>
2025-01-20 16:32:54 +0530

    Add IMAGES/userdata.img to merge_config_other_items_list
    To avoid merging of userdata for tablet builds, create new config along
    with adding userdata image for the list of adding target files.
    
    Add check to load relevant config only for tablet builds.
    
    Change-Id: I66612b6f0c665f46c0a518a73d0b070be871c546
    Signed-off-by: kodanda rami reddy v <quic_kreddyv@quicinc.com>

--- MergeConfig.mk
+++ MergeConfig.mk
@@ -2,8 +2,12 @@
 ifeq ($(ENABLE_AB), true)
     # Handle Case for QSSI-Dynamic Partition
     ifeq ($(PRODUCT_USE_DYNAMIC_PARTITIONS), true)
+            ifeq (,$(filter $(TARGET_BOARD_DERIVATIVE_SUFFIX), _tb))
+                $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/ab/merge_config_other_item_list)
+            else
+                $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/ab/msmnile_tb/merge_config_other_item_list)
+            endif
             $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/ab/merge_config_system_misc_info_keys)
-            $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/ab/merge_config_other_item_list)
             $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/dynamic_partition/ab/merge_config_system_item_list)
     else
         $(call dist-for-goals,droidcore,device/qcom/vendor-common/ota_merge_configs/without_dynamic_partition/ab/merge_config_system_misc_info_keys)
