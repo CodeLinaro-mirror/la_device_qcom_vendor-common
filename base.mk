458d00600e7b215f52be961087df3c9588ab02bf
Kai Meng <quic_kmeng@quicinc.com>
2025-02-14 17:43:32 +0800

    vendor-common: Add lxc script in vendor
    Change-Id: I416c86650c633c296848cb6f762e1d9ec3cbfe78
    Signed-off-by: Kai Meng <quic_kmeng@quicinc.com>

--- base.mk
+++ base.mk
@@ -1085,7 +1085,8 @@ PRODUCT_PACKAGES_DEBUG += \
     init.qti.debug-lito.sh \
     init.qti.debug-atoll.sh \
     init.qti.debug-trinket.sh \
-    init.qti.debug-bengal.sh
+    init.qti.debug-bengal.sh \
+    init.qcom.lxc.sh
 
 PRODUCT_PACKAGES += liboemaids_system
 PRODUCT_PACKAGES += liboemaids_vendor
